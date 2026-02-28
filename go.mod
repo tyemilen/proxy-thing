337941b1916bb69b669cf0cb26943d67f177124d
dokinarieki <dokinarieki@disroot.org>
2026-02-28 20:09:45 +0300

    iwantokms

--- go.mod
+++ go.mod
@@ -4,6 +4,31 @@ go 1.25.7
 
 require (
 	github.com/elazarl/goproxy v1.8.2
+	github.com/icza/backscanner v0.0.0-20241124160932-dff01ac50250
+	github.com/xtls/xray-core v1.260206.0
+	google.golang.org/grpc v1.79.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/apernet/quic-go v0.57.2-0.20260111184307-eec823306178 // indirect
+	github.com/cloudflare/circl v1.6.3 // indirect
+	github.com/juju/ratelimit v1.0.2 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.12 // indirect
+	github.com/miekg/dns v1.1.72 // indirect
+	github.com/pires/go-proxyproto v0.9.2 // indirect
+	github.com/refraction-networking/utls v1.8.2 // indirect
+	github.com/sagernet/sing v0.5.1 // indirect
+	github.com/xtls/reality v0.0.0-20251014195629-e4eec4520535 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/mod v0.32.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
+	golang.org/x/tools v0.41.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+	lukechampine.com/blake3 v1.4.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/elazarl/goproxy"
+	"github.com/icza/backscanner"
+	"github.com/xtls/xray-core/app/proxyman/command"
+	"github.com/xtls/xray-core/common/net"
+	"github.com/xtls/xray-core/common/protocol"
+	"github.com/xtls/xray-core/common/serial"
+	"github.com/xtls/xray-core/core"
+	"github.com/xtls/xray-core/proxy/vless"
+	"github.com/xtls/xray-core/proxy/vless/outbound"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -45,9 +62,29 @@ func main() {
 
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 
+	xrayLog, err := os.Open("/var/log/xray/error.log")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer xrayLog.Close()
+
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 		if resp != nil {
+			// addNew()
 			log.Printf("[%s] %s -> %d %s", ctx.Req.Method, ctx.Req.URL.Host, resp.StatusCode, resp.Status)
+
+			if resp.StatusCode >= 400 {
+				urlStr := ctx.Req.URL.String()
+				tag, err := findProxy(xrayLog, ctx.Req.URL.String())
+
+				if err != nil {
+					log.Printf("err %s -> %s", urlStr, err)
+				} else {
+					log.Printf("%s -> %s", urlStr, tag)
+				}
+			}
 		}
 		return resp
 	})
@@ -55,4 +92,114 @@ func main() {
 	addr := ":1337"
 	log.Printf("%s", addr)
 	log.Fatal(http.ListenAndServe(addr, proxy))
+}
+
+func findProxy(xrayLog *os.File, urlStr string) (string, error) {
+	xStat, err := xrayLog.Stat()
+
+	if err != nil {
+		return "", err
+	}
+
+	var id string
+	var urlPos int
+
+	reId := regexp.MustCompile(`\[Info\]\s+\[([^\]]+)\]`)
+	xSize := xStat.Size()
+	scanner := backscanner.New(xrayLog, int(xSize))
+	for {
+		line, pos, err := scanner.LineBytes()
+		if err != nil {
+			return "", err
+		}
+		strLine := string(line)
+		if strings.Contains(strLine, urlStr) {
+			idMatch := reId.FindStringSubmatch(strLine)
+
+			if len(idMatch) > 1 {
+				id = idMatch[1]
+				urlPos = pos
+				break
+			}
+		}
+	}
+	if len(id) <= 0 {
+		return "", errors.New("id is empty")
+	}
+
+	pattern := fmt.Sprintf(`\[Info\]\s+\[%s\].*?detour\s+\[([^\]]+)\]`, regexp.QuoteMeta(id))
+	reProxy, err := regexp.Compile(pattern)
+
+	if err != nil {
+		return "", errors.New("bad regexp")
+	}
+
+	section := io.NewSectionReader(xrayLog, int64(urlPos), xSize-int64(urlPos))
+	fScanner := bufio.NewScanner(section)
+	linesRead := 0
+	for fScanner.Scan() {
+		if linesRead >= 500 {
+			return "", errors.New("cant find proxy tag")
+		}
+
+		line := fScanner.Text()
+
+		if strings.Contains(line, "["+id+"]") && strings.Contains(line, "taking detour") {
+			proxyMatch := reProxy.FindStringSubmatch(line)
+			if len(proxyMatch) > 1 {
+				proxyTag := proxyMatch[1]
+
+				return proxyTag, nil
+			}
+		}
+
+		linesRead++
+	}
+
+	return "", errors.New("=(")
+}
+
+func addNew() error {
+	conn, err := grpc.NewClient("xray:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+		return err
+	}
+	defer conn.Close()
+
+	client := command.NewHandlerServiceClient(conn)
+
+	out := &outbound.Config{
+		Vnext: &protocol.ServerEndpoint{
+			Address: &net.IPOrDomain{
+				Address: &net.IPOrDomain_Domain{
+					Domain: "domain.com",
+				},
+			},
+			Port: 443,
+			User: &protocol.User{
+				Account: serial.ToTypedMessage(&vless.Account{
+					Id:         "random-uuid",
+					Flow:       "xtls-rprx-vision",
+					Encryption: "none",
+				}),
+			},
+		},
+	}
+	request := &command.AddOutboundRequest{
+		Outbound: &core.OutboundHandlerConfig{
+			Tag:           "test-vless",
+			ProxySettings: serial.ToTypedMessage(out),
+		},
+	}
+
+	r, err := client.AddOutbound(context.Background(), request)
+
+	if err != nil {
+		log.Printf("ERROR Sent: %+v\n", err)
+		return err
+	}
+	log.Printf("Sent: %+v\n", r)
+
+	return nil
 }
